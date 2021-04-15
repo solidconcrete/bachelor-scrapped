da82a9c367628809508f4e5ad27a51364b0aba69
solidconcrete <saniasania523@gmail.com>
2021-04-15 22:15:18 +0300

    init commit

---
+++ bachelor-backend/src/main/java/com/bachelor/bachelorbackend/BachelorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bachelor.bachelorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BachelorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BachelorBackendApplication.class, args);
+    }
+
+}
